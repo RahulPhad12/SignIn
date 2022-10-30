d1fbf7e12070c9228367c531ad5c1989abe9b24e
Rahul Phad <101962301+RahulPhad12@users.noreply.github.com>
2022-10-31 00:38:06 +0530

    Update signin.js

--- src/components/signin.js
+++ src/components/signin.js
@@ -24,7 +24,7 @@ function App() {
     if (Object.keys(formErrors).length === 0 && isSubmit) {
       console.log(formValues);
     }
-  }, [formErrors]);
+  });
   const validate = (values) => {
     const errors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
